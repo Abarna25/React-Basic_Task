62216f8974276da8ca4c7612d30f08687c5ca61b
Abarna25 <abarna.thiyagarajan23@gmail.com>
2025-02-11 22:50:17 +0530

    first commit

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contacts() {
+  return (
+    <div>
+        <p>Contact us for more details!</p>
+    </div>
+  )
+}
+
+export default Contacts
---
+++ src/components/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Services() {
+  return (
+    <div>
+        <p>Services we provide!</p>
+    </div>
+  )
+}
+
+export default Services
